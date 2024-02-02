80d398202cd24445259d1aa072dc8c8381ae6c82
Alexey <alexeykh007@gmail.com>
2024-02-02 23:24:29 +0300

    xe

--- Template4333/Template4333/MainWindow.xaml.cs
+++ Template4333/Template4333/MainWindow.xaml.cs
@@ -30,5 +30,11 @@ namespace Template4333
             _4333_Kulikova win = new _4333_Kulikova();  
             win.Show();
         }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            _4333_KhokhlovAlexey win = new _4333_KhokhlovAlexey();
+            win.Show();
+        }
     }
 }
