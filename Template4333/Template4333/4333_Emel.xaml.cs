59dc025a84b67af777eb8e31e0c369239481f164
juliya <rozaviti@gmail.com>
2024-03-16 15:19:58 +0300

    3 laba

---
+++ Template4333/Template4333/4333_Emel.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Template4333
+{
+    /// <summary>
+    /// Логика взаимодействия для _4333_Emel.xaml
+    /// </summary>
+    public partial class _4333_Emel : Window
+    {
+        public _4333_Emel()
+        {
+            InitializeComponent();
+        }
+    }
+}
